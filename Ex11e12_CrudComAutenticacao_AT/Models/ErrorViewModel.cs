c6646dbcdce193061c55b33e5b48619620b2d15b
FeHunter <hunterfe1111@gmail.com>
2024-09-22 15:02:49 -0300

    Adicionar arquivos de projeto.

---
+++ Ex11e12_CrudComAutenticacao_AT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ex11e12_CrudComAutenticacao_AT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
